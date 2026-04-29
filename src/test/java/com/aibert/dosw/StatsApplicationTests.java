c0d45d23ac8da63c363d6d08cd926a3389b2236f
Juan Lopez <juan.lbarrera@mail.escuelaing.edu.co>
2026-04-28 19:13:30 -0500

    feature/pipelineCI se realizaron los mismos cambios y las mismas implementaciones para la CI que en los repos anteriores juan lopez :dragon:

---
+++ src/test/java/com/aibert/dosw/StatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibert.dosw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
